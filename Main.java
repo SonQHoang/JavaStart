688c9c4de803ebb2f36098248bc47fceff75ca2e
Son Hoang <sonquhoang@gmail.com>
2023-11-26 21:28:08 -0800

    Nuances with scanner for user input

--- Main.java
+++ Main.java
@@ -89,20 +89,40 @@
 //     }
 // }
 
+// public class Main {
+//     public static void main(String[] args) {
+//         String x = "Pizza is the best!";
+
+//         String y = "Steak is the better option...";
+
+//         String temp;
+
+//         temp = x;
+//         x = y;
+//         y = temp;
+
+//         System.out.println("This is x: "+x);
+
+//         System.out.println("This is y: "+y);
+//     }
+// }
+import java.util.Scanner; // Importing the scanner class
+
 public class Main {
-    public static void main(String[] args) {
-        String x = "Pizza is the best!";
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in); //Utilizing scanner to have user input
 
-        String y = "Steak is the better option...";
+        System.out.println("What is your name? ");
+        String name = scanner.nextLine(); // Part of scanner
+        System.out.println("How old are you? ");
+        int age = scanner.nextInt();
+        scanner.nextLine(); // clears the scanner;
+        System.out.println("What is your favorite food?");
+        String food = scanner.nextLine();
 
-        String temp;
 
-        temp = x;
-        x = y;
-        y = temp;
-
-        System.out.println("This is x: "+x);
-
-        System.out.println("This is y: "+y);
+        System.out.println("Hello " + name);
+        System.out.println("You are "+age+ " years old");
+        System.out.println("You like " + food);
     }
 }
